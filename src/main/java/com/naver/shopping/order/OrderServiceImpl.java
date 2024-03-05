d45ddc700f2d2fd942e20881a1ec4dbe35a23032
kimbob <axzsd96301@gmail.com>
2024-03-06 00:09:45 +0900

    Add auto config by Component Scan

--- src/main/java/com/naver/shopping/order/OrderServiceImpl.java
+++ src/main/java/com/naver/shopping/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package com.naver.shopping.order;
 import com.naver.shopping.member.Member;
 import com.naver.shopping.member.MemberRepository;
 import com.naver.shopping.discount.DiscountPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
