e837854da4f6d6c13415246560c02a839f4d5c66
KIMB0B <axzsd96301@gmail.com>
2024-03-12 16:11:23 +0900

    Learn Bean's Lifecycle

---
+++ src/test/java/com/naver/shopping/lifecycle/LifeCycleConfig.java
@@ -0,0 +1,15 @@
+package com.naver.shopping.lifecycle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LifeCycleConfig {
+
+    @Bean
+    public NetworkClient networkClient() {
+        NetworkClient networkClient = new NetworkClient();
+        networkClient.setUrl("https://www.github.com");
+        return networkClient;
+    }
+}
